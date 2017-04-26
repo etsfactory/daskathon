60818d29112676b8453c9c7a51138d08d7240c7a
Szucs Krisztian <szucs.krisztian@gmail.com>
2017-04-26 09:56:43 +0200

    fixed no token auth

--- daskathon/cli.py
+++ daskathon/cli.py
@@ -169,8 +169,8 @@ def deploy(marathon, name, docker, scheduler_cpus, scheduler_mem, adaptive,
     client.create_app('{}-scheduler'.format(name), scheduler)
 
     if jupyter:
-        cmd = ('jupyter notebook --allow-root --no-browser --token \'\' '
-               '--ip 0.0.0.0 --port $PORT_NOTEBOOK')
+        cmd = ('jupyter notebook --allow-root --no-browser '
+               '--NotebookApp.token=\'\' --ip 0.0.0.0 --port $PORT_NOTEBOOK')
         ports = [{'port': 0, 'protocol': 'tcp', 'name': 'notebook'}]
         jupyter = deepcopy(scheduler)
         jupyter.cmd = cmd
